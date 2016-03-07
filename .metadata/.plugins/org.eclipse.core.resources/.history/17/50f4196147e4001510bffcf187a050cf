72ca6523ae7c9e7e4b8edf8f1d10e33a6be80aaa
= <=>
2016-03-07 11:37:09 +0200

    Initializations callbacks with implement the Interface

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/17/50f4196147e4001510bffcf187a050cf
@@ -0,0 +1,15 @@
+package com.cimpapps.hello;
+
+public class HelloWorld {
+	String message;
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
--- HelloSpring/src/com/cimpapps/hello/Main.java
+++ HelloSpring/src/com/cimpapps/hello/Main.java
@@ -6,24 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		
-		//Demo of Singleton Scope...we will have 2 ref vars pointing to the same object
-		//the getBean() method will return each time same object
 		HelloWorld singletonObj1 = (HelloWorld) context.getBean("helloWorld");
-		singletonObj1.setMessage("Hello World!");
-		HelloWorld singletonObj2 = (HelloWorld) context.getBean("helloWorld");
 		System.out.println(singletonObj1.getMessage());
-		System.out.println(singletonObj2.getMessage());
-		
-		//Demo of Prototype Scope...we will have 2 ref vars pointing to different 2 objects
-		//this time the getBean() method will return each time a new Object
-		HelloWorld prototypeObj1 = (HelloWorld) context.getBean("salutLume");
-		prototypeObj1.setMessage("Salut Lume!");
-		HelloWorld prototypeObj2 = (HelloWorld) context.getBean("salutLume");
 	
-		System.out.println(prototypeObj1.getMessage());
-		System.out.println(prototypeObj2.getMessage());
 	
 	}
 }
